b44c49d6169f76ca4f93624945266143fb325a86
Ümit UĞURLU <umitugurlu02@gmail.com>
2020-05-19 01:05:07 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,23 @@
-/**
- * Oyunun kuralları
- * 
- *
- *Sonradan eklenen yorumlar. 19 Mayıs 2020
- *
- 
- * Bir oyuncu toplam sıfır ile başlar.
- * 
- * Her turda 1 ile 10 arasında rastgele bir sayı üretilir ve oyuncunun toplamına eklenir.
- * 
- * Oyuncunun toplamı 33'ü aşarsa, oyuncu kaybeder ve oyun sona erer: Game Over.
- * 
- * Her turun sonunda oyuncuya başka bir tur tamamlamak veya çıkmak isteyip istemedikleri sorulur.
- * 
- * Çıkarlarsa, oyuncuya aşağıdaki gibi bir puan verilir:
- * 
- * Toplamları 24'ün altında veya 33'ün üzerindeyse Game OVer
- * 
- * Toplamları 24 ila 33 arasındaysa 10 x (toplam - 23) puan.
- * 
- * Örneğin, toplam 29 ise oyuncu, 10 x (29-23) = 60 puan alır. Toplam 33'e ulaştıklarında ulaşabilecekleri maksimum puan 10 x (33-23) = 100'dür!
- */
-let state = true
+
+// Oyunun kuralları
+// 
+// Bir oyuncu toplam sıfır ile başlar.
+// 
+// Her turda 1 ile 10 arasında rastgele bir sayı üretilir ve oyuncunun toplamına eklenir.
+// 
+// Oyuncunun toplamı 33'ü aşarsa, oyuncu kaybeder ve oyun sona erer: Game Over.
+// 
+// Her turun sonunda oyuncuya başka bir tur tamamlamak veya çıkmak isteyip istemedikleri sorulur.
+// 
+// Çıkarlarsa, oyuncuya aşağıdaki gibi bir puan verilir:
+// 
+// Toplamları 24'ün altında veya 33'ün üzerindeyse Game OVer
+// 
+// Toplamları 24 ila 33 arasındaysa 10 x (toplam - 23) puan.
+// 
+// Örneğin, toplam 29 ise oyuncu, 10 x (29-23) = 60 puan alır. Toplam 33'e ulaştıklarında ulaşabilecekleri maksimum puan 10 x (33-23) = 100'dür!
 let score = 0
+let state = true
 while (score < 34 && state) {
     basic.showString("?")
     basic.pause(200)
